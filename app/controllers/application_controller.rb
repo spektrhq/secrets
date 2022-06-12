f180ff86e473891c8077a9017bdb2c5a8e712164
Greg Molnar <molnargerg@gmail.com>
2022-06-12 19:14:15 +0200

    add meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :default_meta_tags
+
+  private
+
+  def default_meta_tags
+    share = {
+      title: 'Securely sharing secrets never been this easy!', image: helpers.image_url('screenshot.png')
+    }
+    {
+      title: 'Securely sharing secrets never been this easy! | Spektr Secrets',
+      twitter: share,
+      og: share
+    }
+  end
 end
