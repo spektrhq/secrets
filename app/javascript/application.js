1e8a560ffc71e48344cfb1c9b40f5882fdf917b5
Greg Molnar <molnargerg@gmail.com>
2022-06-12 13:38:25 +0200

    add clipboardjs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import ClipboardJS from "clipboard"
+
+document.addEventListener('turbo:load', function(){
+  new ClipboardJS('.clipboard');
+}, false);
